30e7fad36511752707ab5a3cbd4c6f0b637b1d58
Carlos Oporto <carlosoporto@gmail.com>
2019-05-28 23:18:33 -0500

    added Wunbit Token and DetailedToken tests

--- contracts/WunbitToken.sol
+++ contracts/WunbitToken.sol
@@ -1,10 +1,15 @@
 pragma solidity ^0.5.2;
 
-import "openzeppelin-solidity/contracts/token/ERC20/ERC20Detailed.sol";
 import "openzeppelin-solidity/contracts/token/ERC20/ERC20.sol";
+import "openzeppelin-solidity/contracts/token/ERC20/ERC20Detailed.sol";
 
 contract WunbitToken is ERC20, ERC20Detailed {
-  constructor (string memory name, string memory symbol, uint8 decimals) ERC20Detailed(name, symbol, decimals) public {
-
-  }
+  constructor (
+    string memory name,
+    string memory symbol,
+    uint8 decimals
+  )
+    ERC20Detailed(name, symbol, decimals)
+    public
+  {}
 }
---
+++ test/WunbitToken.test.js
@@ -0,0 +1,33 @@
+const { BN } = require('openzeppelin-test-helpers');
+
+const WunbitToken = artifacts.require('WunbitToken');
+
+require('chai')
+  .should();
+
+contract('WunbitToken', accounts => {
+  const _name = 'Wunbit Token';
+  const _symbol = 'WUN';
+  const _decimals = new BN(18);
+
+  beforeEach(async function() {
+    this.token = await WunbitToken.new(_name, _symbol, _decimals);
+  });
+
+  describe('token attributes', function() {
+    it('has the correct name', async function() {
+      const name = await this.token.name();
+      name.should.equal(_name);
+    });
+
+    it('has the correct symbol', async function() {
+      const symbol = await this.token.symbol();
+      symbol.should.equal(_symbol);
+    });
+
+    it('has the correct decimals', async function() {
+      const decimals = await this.token.decimals();
+      decimals.should.be.bignumber.equal(_decimals);
+    });
+  });
+});
